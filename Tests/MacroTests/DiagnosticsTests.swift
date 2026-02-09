0e7acc39cffe41eb9b02881e4f5b3f7559a5c57b
Wolfgang Lutz <wlut@num42.de>
2026-02-09 13:12:30 +0100

    add tests

---
+++ Tests/MacroTests/DiagnosticsTests.swift
@@ -0,0 +1,48 @@
+import MacroTester
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
+import Testing
+
+#if canImport(AutoDefaultsSettingMacros)
+  import AutoDefaultsSettingMacros
+
+  @Suite struct AutoDefaultsSettingDiagnosticsTests {
+    let testMacros: [String: Macro.Type] = [
+      "AutoDefaultsSetting": AutoDefaultsSettingMacro.self
+    ]
+
+    @Test func requiresThreeArguments() {
+      assertMacroExpansion(
+        """
+        #AutoDefaultsSetting(key: "onlyKey", type: String.self)
+        """,
+        expandedSource: "",
+        diagnostics: [
+          .init(
+            message: AutoDefaultsSettingMacro.MacroDiagnostic.requiresThreeArguments.message,
+            line: 1,
+            column: 1
+          )
+        ],
+        macros: testMacros
+      )
+    }
+
+    @Test func interpolatedKeyThrowsError() {
+      assertMacroExpansion(
+        """
+        #AutoDefaultsSetting(key: "launch\\(1)", type: Int.self, default: 0)
+        """,
+        expandedSource: "",
+        diagnostics: [
+          .init(
+            message: AutoDefaultsSettingMacro.MacroDiagnostic.keyMustBePlainStringLiteral.message,
+            line: 1,
+            column: 1
+          )
+        ],
+        macros: testMacros
+      )
+    }
+  }
+#endif
